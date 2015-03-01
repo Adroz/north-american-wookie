0dca813fd28a7473f5e46448dc2f71a916a52d7e
Adroz <nicholas.k.moores@gmail.com>
2015-03-01 10:26:16 +1000

    Project rename

--- pca10028/s110/arm5/RTE/RTE_Components.h
+++ pca10028/s110/arm5/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'ble_app_template_s110_pca10028' 
+ * Project: 'ble_app_wookie' 
  * Target:  'nrf51422_xxac_s110' 
  */
 
